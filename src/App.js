5bd0aebacd43603713e344dbd7cfd027e474ac39
Leavemydogalone <niklasport88@gmail.com>
2020-12-10 11:02:34 -0800

    added subscription to firebase in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './components/Post';
+import firebase from './firebase';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const ref = firebase.firestore().collection('posts');
+
+  function getPosts() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+        setPosts(items);
+        setLoading(false);
+      });
+    });
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div className="App" data-testid="App">
       <div className="topBar">top bar y</div>
