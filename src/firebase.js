b9c5d6ff02860757ae0af6b0ef126497359d1fe5
Leavemydogalone <niklasport88@gmail.com>
2020-12-17 11:58:24 -0800

    eee

--- src/firebase.js
+++ src/firebase.js
@@ -10,16 +10,6 @@ const firebaseConfig = {
   measurmentId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// var firebaseConfig = {
-//   apiKey: 'AIzaSyCWQSUVn3zb_D3X2hKWZmjlIcEzFYjUYW8',
-//   authDomain: 'redrit-75871.firebaseapp.com',
-//   projectId: 'redrit-75871',
-//   storageBucket: 'redrit-75871.appspot.com',
-//   messagingSenderId: '439515708143',
-//   appId: '1:439515708143:web:8d2173cad9d3504e85391a',
-//   measurementId: 'G-BHLNZNRTVE',
-// };
-
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
